d4130fd2fbf98db03b1d60772b65479181aadc27
Himanshu Gupta <himanshugupta714@gmail.com>
2024-12-11 01:32:27 +0530

    feat(comboBox): comboBox component is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import "./App.scss";
-import { ComboBox } from "./combo-box";
+import { useState } from 'react';
+import { comboBoxOptions, ComBoxOptionsEnum } from './App.constants';
+import './App.scss';
+import ComboBox from './components/ComboBox';
+import { ComboBoxOption } from './components/ComboBox/ComboBox';
 
 const App = () => {
+  const [selectedOptions, setSelectedOptions] = useState<
+    ComboBoxOption<ComBoxOptionsEnum>[]
+  >([]);
+
   return (
-    <div className="center">
-      <div className="card">
-        <ComboBox />
+    <div className='center'>
+      <div className='card'>
+        <ComboBox
+          label='Combobox Dropdown'
+          options={comboBoxOptions}
+          setSelectedOptions={setSelectedOptions}
+          selectedOptions={selectedOptions}
+        />
       </div>
     </div>
   );
---
+++ src/hooks/useClickAway.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+export const useClickAway = ({
+  ref,
+  handler,
+}: {
+  ref: React.RefObject<HTMLDivElement>;
+  handler: (event: MouseEvent | TouchEvent) => void;
+}) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
